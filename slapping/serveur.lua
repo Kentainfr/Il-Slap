bf5f9449e5e60168a450e9bfd721798e9af980ce
Kentain_fr <63867494+Kentainfr@users.noreply.github.com>
2021-05-26 22:51:35 +0200

    Add files via upload

---
+++ slapping/serveur.lua
@@ -0,0 +1,13 @@
+RegisterServerEvent('RebornProject:SyncSon_Serveur')
+AddEventHandler('RebornProject:SyncSon_Serveur', function()
+    TriggerClientEvent('RebornProject:SyncSon_Client', -1, source)
+end)
+
+RegisterServerEvent('RebornProject:SyncGiffle')
+AddEventHandler('RebornProject:SyncGiffle', function(netID)
+    TriggerClientEvent('RebornProject:SyncAnimation', netID)
+end)
+
+
+-- Discord link : https://discord.gg/EDRbwTEpDu
+-- Author : Kentain
